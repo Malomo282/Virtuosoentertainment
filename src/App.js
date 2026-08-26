be597945636f0601e269bc1656145795d141b2fc
Malomo282 <jesseappiah28@gmail.com>
2026-08-26 09:53:00 +0100

    Revert "Trigger redeploy"
    This reverts commit 377738c2e56c5454e3356062d66096c05a275954.

--- src/App.js
+++ src/App.js
@@ -2871,7 +2871,6 @@ export default function App() {
   const [page, setPage] = useState(() => hashToPage(window.location.hash));
   const [contactDropdown, setContactDropdown] = useState(false);
   const [mobileContactOpen, setMobileContactOpen] = useState(false);
-  // Trigger redeploy
 
   const setPageAndScroll = (p) => {
     setContactDropdown(false);
